9d7d30a1d5bc192ccdda375a134b78a3a036889d
Junxiao Shi <git@mail1.yoursunny.com>
2021-08-17 21:30:59 -0400

    format code style

--- consumer.mjs
+++ consumer.mjs
@@ -1,4 +1,4 @@
-import { openUplinks, closeUplinks } from "@ndn/cli-common";
+import { closeUplinks, openUplinks } from "@ndn/cli-common";
 import { Endpoint } from "@ndn/endpoint";
 import { Interest } from "@ndn/packet";
 
@@ -6,26 +6,26 @@ import { Interest } from "@ndn/packet";
 const [x, y] = process.argv.slice(2);
 
 (async () => {
-  // Connect to NFD.
-  await openUplinks();
+// Connect to NFD.
+await openUplinks();
 
-  const endpoint = new Endpoint();
+const endpoint = new Endpoint();
 
-  // Make an Interest packet, asking the producer to compute x+y.
-  const interest = new Interest(`/add/${x}/${y}`);
-  interest.mustBeFresh = true;
+// Make an Interest packet, asking the producer to compute x+y.
+const interest = new Interest(`/add/${x}/${y}`);
+interest.mustBeFresh = true;
 
-  try {
-    // Send the Interest, and wait for Data to come back.
-    const data = await endpoint.consume(interest, { retx: 100 });
+try {
+  // Send the Interest, and wait for Data to come back.
+  const data = await endpoint.consume(interest, { retx: 100 });
 
-    // Print the Data payload.
-    process.stdout.write(data.content);
-  } catch (err) {
-    // In case of Data retrieval failure, show what went wrong.
-    console.warn(err);
-  }
+  // Print the Data payload.
+  process.stdout.write(data.content);
+} catch (err) {
+  // In case of Data retrieval failure, show what went wrong.
+  console.warn(err);
+}
 
-  // Disconnect from NFD, so that Node.js can exit normally.
-  closeUplinks();
+// Disconnect from NFD, so that Node.js can exit normally.
+closeUplinks();
 })();
--- producer.mjs
+++ producer.mjs
@@ -1,46 +1,47 @@
 import { openUplinks } from "@ndn/cli-common";
 import { Endpoint } from "@ndn/endpoint";
-import { Name, Data } from "@ndn/packet";
+import { Data } from "@ndn/packet";
 
 (async () => {
-  // openUplinks() creates a connection to the "uplink", in this case the local NFD forwarder.
-  // It returns a Promise, so remember to await it.
-  await openUplinks();
+// openUplinks() creates a connection to the "uplink", in this case the local NFD forwarder.
+// It returns a Promise, so remember to await it.
+await openUplinks();
 
-  // Endpoint is a centerpiece of NDNts. You can use it to create a producer or a consumer.
-  // It is similar to, but more powerful than, "face" in other NDN libraries.
-  // You'll soon see some of its powers.
-  const endpoint = new Endpoint();
+// Endpoint is a centerpiece of NDNts. You can use it to create a producer or a consumer.
+// It is similar to, but more powerful than, "face" in other NDN libraries.
+// You'll soon see some of its powers.
+const endpoint = new Endpoint();
 
-  // endpoint.produce() creates a producer.
-  // The first argument is the name prefix.
-  // The second argument is a callback function that is invoked for each incoming Interest;
-  // this must be an async function that returns a Promise.
-  endpoint.produce(new Name("/add"), async (interest) => {
-    console.log(`Got Interest ${interest.name}`);
-    // This producer is a calculator. It expects Interest name to have three
-    // components: "add", x, and y. If it's not, reject the Interest.
-    if (interest.name.length !== 3) {
-      console.log("Wrong name length.");
-      return;
-    }
+// endpoint.produce() creates a producer.
+// The first argument is the name prefix.
+// The second argument is a callback function that is invoked for each incoming Interest;
+// this must be an async function that returns a Promise.
+endpoint.produce("/add", async (interest) => {
+  console.log(`Got Interest ${interest.name}`);
+  // This producer is a calculator. It expects Interest name to have three
+  // components: "add", x, and y. If it's not, reject the Interest.
+  if (interest.name.length !== 3) {
+    console.log("Wrong name length.");
+    return;
+  }
 
-    // Extract x and y numbers, then compute the sum.
-    const x = Number.parseInt(interest.name.at(1).text);
-    const y = Number.parseInt(interest.name.at(2).text);
-    const sum = x + y;
-    console.log(`${x} + ${y} = ${sum}`);
+  // Extract x and y numbers, then compute the sum.
+  const x = Number.parseInt(interest.name.at(1).text, 10);
+  const y = Number.parseInt(interest.name.at(2).text, 10);
+  const sum = x + y;
+  console.log(`${x} + ${y} = ${sum}`);
 
-    // Make a Data packet that has the same name as the Interest.
-    const data = new Data(interest.name);
-    data.freshnessPeriod = 1000;
-    data.content = new TextEncoder().encode(`${sum}\n`);
+  // Make a Data packet that has the same name as the Interest.
+  const data = new Data(interest.name);
+  data.freshnessPeriod = 1000;
+  data.content = new TextEncoder().encode(`${sum}\n`);
 
-    // Simulate 100 milliseconds processing delay.
-    await new Promise((r) => setTimeout(r, 100));
+  // Simulate 100 milliseconds processing delay.
+  await new Promise((r) => setTimeout(r, 100));
 
-    // Sending the Data is as simple as returning it from the function.
-    return data;
-  },
-  { concurrency: 10 });
+  // Sending the Data is as simple as returning it from the function.
+  return data;
+},
+{ concurrency: 10 },
+);
 })();
